146e328a11a27579e0b4fcd7d54c21fdc40a5fc1
Greg Williams <gregwilliams@Gregs-MacBook-Air.local>
2019-07-03 19:48:23 -0400

    updated main-pagev2

--- java.js
+++ java.js
@@ -1,26 +1,29 @@
-
 //  Code for button  //
-
-const selectButton = document.querySelector('button.pick');
-
-selectButton.addEventListener ('click', () => {
-    var lookup = document.querySelector('#Char-select');
-    
-    alert(lookup);
+const selectButton = document.querySelector('.pick');
 
 
-})
+selectButton.addEventListener('click', function() {
+    var lookup = document.querySelector('#Char-select').value;
+    window.open(lookup);
+});
+
+
+
+
+// /code for button  //
+
+
 
 //  adhoc code  //
 
-function update(previewPic){
+function update(previewPic) {
     var src = previewPic.getAttribute("Characters/Alex.html");
     var alt = previewPic.getAttribute("Preview of the Characters website");
     document.getElementById('Alex').style.backgroundImage = URL(src);
     document.getElementById('Alex').innerHTML = alt;
 }
 
-function unDo(){
+function unDo() {
     document.getElementById('Alex').style.backgroundImage = "none";
     document.getElementById('Alex').innerHTML = "MouseOver Image to Display Here";
 }
